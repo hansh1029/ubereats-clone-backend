b424cd4548371b8e1d10abdd2f7c07710fa896c6
hansh1029 <hansh1029@gmail.com>
2021-03-22 23:31:02 +0900

    create account mutation 3 resolver added

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,20 +1,40 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
-import { CreateAccountInput, CreateAccountOutput } from './dtos/create-account.dto';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
-@Resolver(of => User)
+@Resolver((of) => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
-  @Query(returns => Boolean)
+  @Query((returns) => Boolean)
   hi() {
     return true;
   }
 
-  @Mutation(returns => CreateAccountOutput)
-  createAccount(@Args("input") createAccountInput: CreateAccountInput) {
-      
-
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput);
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
